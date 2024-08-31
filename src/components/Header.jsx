55787f5e670b0bec9c9656596061c3e33435bbd7
sahumahesh0512 <91590622+sahumahesh0512@users.noreply.github.com>
2024-08-31 17:30:49 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
   return (
     <div className="h-16 px-3 text-3xl bg-darkBlue1 text-slate-300 w-full flex items-center border-b-2 border-lightGray">
       <div className="flex-grow font-semibold">
-        <Link to="/">Sonic Meet</Link>
+        <Link to="/">K-Meet</Link>
       </div>
       <div>
         {user ? (
